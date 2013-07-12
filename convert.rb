d801df094688b24072905290f9342af2dc3ee2ff
Brad Midgley <bmidgley@gmail.com>
2013-07-11 21:40:49 -0600

    folder bands

--- convert.rb
+++ convert.rb
@@ -3,7 +3,6 @@ require 'csv'
 
 src = "rptrraw.txt"
 #system "rm #{src}; wget 'http://www.ussc.com/~uvhfs/#{src}'"
-counter = 1
 puts '<?xml version="1.0" encoding="UTF-8"?>'
 puts '<kml xmlns="http://www.opengis.net/kml/2.2">'
 puts ' <Document>'
@@ -11,6 +10,7 @@ puts '  <name>Utah Repeaters</name>'
 puts '  <open>1</open>'
 puts '  <description>Utah Repeaters from UVHFS</description>'
 puts '  <LookAt><longitude>-111.3644</longitude><latitude>39.6181</latitude><altitude>2500</altitude><heading>0</heading><tilt>0</tilt><range>900000</range></LookAt>'
+bands = {}
 CSV.foreach(src, :encoding => 'windows-1251:utf-8', :headers => true) do |row|
   next if row['LATITUDE'].to_f == 0
   next if row['LONGITUDE'].to_f == 0
@@ -18,17 +18,24 @@ CSV.foreach(src, :encoding => 'windows-1251:utf-8', :headers => true) do |row|
   next unless row['OPEN'] == 'Y'
   next if row['CLOSED'] == 'Y'
   next if row['EXPERIMENTAL'] == 'Y'
-  #next unless ['W7SP'].include? row['CALLSIGN']
-  puts "  <Placemark id=\"#{counter}\">"
-  puts "   <name>#{row['Site Name']}</name>"
-  puts "   <description>#{row['CALLSIGN']} serving: #{row['Coverage Area']} link: #{row['LINK_FREQ']}</description>"
-  puts "   <Point><coordinates>#{row['LONGITUDE']},#{row['LATITUDE']},0</coordinates></Point>"
-  puts '  </Placemark>'
-  counter += 1
+  (bands[row['BAND']] ||= []) << row
+end
+bands.each_pair do |k,v|
+  puts "  <Folder id=\"#{k}\">"
+  puts "   <name>#{k} Mhz</name>"
+  v.each do |row|
+    puts "   <Placemark>"
+    puts "    <name>#{row['Site Name']}</name>"
+    puts "    <description>#{row['CALLSIGN']} #{row['OUTPUT']}(#{row['INPUT']}@#{row['CTCSS_IN']}#{row['DCS_CODE']})</description>"
+    puts "    <Point><coordinates>#{row['LONGITUDE']},#{row['LATITUDE']},0</coordinates></Point>"
+    puts '   </Placemark>'
+  end
+  puts "  </Folder>"
 end
 puts ' </Document>'
 puts '</kml>'
 
+# row sample
 # {"BAND":"144" "OUTPUT":"146.7600" "INPUT":"146.1600" "STATE":"UT" "LOCATION":"Provo" "CALLSIGN":"W7SP" "SPONSOR":"UARC" "SOURCE":"UVHFS" "AREA":"Wasatch Front"
 # "COORDINATED":"Y" "OPEN":"Y" "CLOSED":"N" "BILINGUAL":nil "EXPERIMENTAL":"N" "LITZ":"N" "TONE":"N" "CTCSS_IN":nil "CTCSS_OUT":nil "DCS":"N" "DCS_CODE":nil "DTMF":"N"
 # "REMOTE_BASE":"N" "SNP":"N" "AUTOPATCH":"Y" "PATCH_SEQ":nil "CLOSED_PATCH":"Y" "EMERG_POWER":"Y" "EMERG_SUN":"N" "EMERG_WIND":"N" "LINKED":"N" "LINK_FREQ":nil
